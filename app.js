7e5217e1831db4c1c6657e29ad2a2357ed92e94a
Jisoon <siriusda2@naver.com>
2020-12-09 12:17:29 +0900

    20201209

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ class UI {
 					// display cart item
 					this.addCartItem(cartItem);
 					// show the cart
+					this.showCart();
 				})
 			}
 		})
@@ -101,7 +102,39 @@ class UI {
 		cartItems.innerText = itemsTotal;
 	}
 	addCartItem(item) {
-
+		const div = document.createElement('div');
+		div.classList.add('cart-item');
+		div.innerHTML = `
+			<img src=${item.image} alt="">
+			<div>
+				<h4>${item.title}</h4>
+				<h5>${item.price}</h5>
+				<span class="remove-item" data-id=${item.id}>remove</span>
+			</div>
+			<div>
+				<i class="fas fa-chevron-up" data-id=${item.id}></i>
+				<p class="item-amount">${item.amount}</p>
+				<i class="fas fa-chevron-down" data-id=${item.id}></i>
+			</div>`
+		cartContent.appendChild(div);
+	}
+	showCart() {
+		cartOverlay.classList.add('transparentBcg');
+		cartDOM.classList.add('showCart');
+	}
+	setupAPP() {
+		cart = Storage.getCart();
+		this.setCartValues(cart);
+		this.populateCart(cart);
+		cartBtn.addEventListener('click', this.showCart);
+		closeCartBtn.addEventListener('click', this.hideCart);
+	}
+	populateCart(cart) {
+		cart.forEach(item => this.addCartItem(item));
+	}
+	hideCart() {
+		cartOverlay.classList.remove('transparentBcg');
+		cartDOM.classList.remove('showCart');
 	}
 }
 // local storage
@@ -116,12 +149,17 @@ class Storage {
 	static saveCart() {
 		localStorage.setItem('cart', JSON.stringify(cart));
 	}
+	static getCart() {
+		return localStorage.getItem('cart') ? 
+			JSON.parse(localStorage.getItem('cart')) : [];
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 	const ui = new UI();
 	const products = new Products();
-
+	// setup app
+	ui.setupAPP();
 	// get all products
 	products.getProducts()
 		.then(products => {
